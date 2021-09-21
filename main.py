5c28a54aeb2913b9246292507d797ce682803cd2
AvaMarquette23 <817328@mystma.org>
2021-09-21 14:31:09 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return(a, b, c, d, e)
+topFiveMovies("Ready Player One", "The Princess Bride", "Harry Potter", "Free Guy", "Hitchhiker's Guide to the Galaxy")
